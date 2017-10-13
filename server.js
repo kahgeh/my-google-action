8deeabd3f777e94b3a412599d877484d591b06d7
anchen li <anchen.li@xero.com>
2017-10-13 11:22:48 +1100

    init

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 const ApiAiAssistant = require('actions-on-google').ApiAiAssistant;
 const bodyParser = require('body-parser');
 const request = require('request');
+const rp = require('request-promise-native');
+const Rx = require('rxjs');
 const app = express();
 const Map = require('es6-map');
 
@@ -15,7 +17,7 @@ const prettyjson = require('prettyjson');
 // https://github.com/epeli/underscore.string#tosentencearray-delimiter-lastdelimiter--string
 const toSentence = require('underscore.string/toSentence');
 
-app.use(bodyParser.json({type: 'application/json'}));
+app.use(bodyParser.json({ type: 'application/json' }));
 
 // This boilerplate uses Express, but feel free to use whatever libs or frameworks
 // you'd like through `package.json`.
@@ -46,53 +48,144 @@ app.get("/", function (request, response) {
 });*/
 
 // Handle webhook requests
-app.post('/', function(req, res, next) {
+app.post('/', function (req, res, next) {
   // Log the request headers and body, to aide in debugging. You'll be able to view the
   // webhook requests coming from API.AI by clicking the Logs button the sidebar.
-  logObject('Request headers: ', req.headers);
-  logObject('Request body: ', req.body);
-    
+  // logObject('Request headers: ', req.headers);
+  // logObject('Request body: ', req.body);
+
   // Instantiate a new API.AI assistant object.
-  const assistant = new ApiAiAssistant({request: req, response: res});
+  const assistant = new ApiAiAssistant({ request: req, response: res });
 
   // Declare constants for your action and parameter names
-  const ASK_WEATHER_ACTION = 'askWeather';  // The action name from the API.AI intent
-  const CITY_PARAMETER = 'geo-city'; // An API.ai parameter name
+  const WELCOME_INPUT = 'input.welcome';
+  const COUNT_NUM = 'count.number';
+  const COUNT_TOTAL = 'count.totalAmount';
+  const CREATE_SPEND = 'create.spend';
+  const CREATE_SEPND_YES = 'create.spend.createspendmoneyforthem-yes';
 
-  // Create functions to handle intents here
-  function getWeather(assistant) {
-    console.log('Handling action: ' + ASK_WEATHER_ACTION);
-    let city = assistant.getArgument(CITY_PARAMETER);
-    
-    // Make an API call to fetch the current weather in the requested city.
-    // See https://developer.yahoo.com/weather/
-    let weatherRequestURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast" +
-        "%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" +
-        encodeURIComponent(city) +
-        "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-    
-    request(weatherRequestURL, function(error, response) {
-      if(error) {
+  function welcome(assistant) {
+    assistant.ask('Welcome to my Wolff agent!')
+  }
+
+  function countNumber(assistant) {
+    let file = assistant.getArgument('file');
+    let category = assistant.getArgument('category');
+
+    request('http://54.69.169.144:5001/api/v1/workingNotes', function (error, response) {
+      if (error) {
         next(error);
-      } else {        
+      } else {
         let body = JSON.parse(response.body);
-        logObject('Weather API call response: ', body);
-        
-        let units = body.query.results.channel.units.temperature == 'F' ? 'Fahrenheit' : 'Celsius';
-        let temperature = body.query.results.channel.item.condition.temp;
-        
-        // Respond to the user with the current temperature.
-        assistant.tell('The current temperature in ' + city + ' is ' + temperature + ' degrees ' + units);
+        const count = body.reduce(function (acc, cur) {
+
+          if (cur.tags.find(function (tag) {
+              return (tag.subject === 'categories' && tag.value === category);
+            })) {
+            acc = acc + cur.files.length;
+          }
+
+          return acc;
+        }, 0);
+
+        assistant.ask('You have ' + count + ' ' + file + ' for ' + category);
       }
     });
   }
-  
-  // Add handler functions to the action router.
+
+  function countTotal(assistant) {
+    assistant.ask('count total!')
+  }
+
+  function createSpend(assistant) {
+    let getCountOfReceiptCategoryContext = assistant.getContext('getcountofreceiptcategory-followup');
+    let count = 0;
+    request('http://54.69.169.144:5001/api/v1/workingNotes', function (error, response) {
+      if (error) {
+        next(error);
+      } else {
+        let body = JSON.parse(response.body);
+        count = body.reduce(function (acc, cur) {
+
+          if (cur.tags.find(function (tag) {
+              return (tag.subject === 'categories' && tag.value === getCountOfReceiptCategoryContext.category);
+            })) {
+            acc = acc + cur.files.length;
+          }
+
+          return acc;
+        }, 0);
+        const googleRichResponse = assistant.buildRichResponse()
+          .addSimpleResponse('Are you sure you want to create ' + count + ' spend monies?')
+          .addSuggestions(
+            ['Yes', 'No'])
+          // Create a basic card and add it to the rich response
+          .addBasicCard(assistant.buildBasicCard('Are you sure you want to create ' + count + ' spend monies?') // Note the two spaces before '\n' required for a
+          // line break to be rendered in the card
+            .setTitle('Create Spend Monies')
+            .setImage('https://www.xero.com/content/dam/xero/images/features/expenses/features-illustrations-mobile-reciept.svg',
+              'Picture of receipt'));
+        assistant.ask(googleRichResponse)
+      }
+    });
+
+  }
+
+  function createSpendYes(assistant) {
+    let getCountOfReceiptCategoryContext = assistant.getContext('getcountofreceiptcategory-followup');
+    request('http://54.69.169.144:5001/api/v1/workingNotes', function (error, response) {
+      if (error) {
+        next(error);
+      } else {
+        let body = JSON.parse(response.body);
+        const targetedDocuments = body.reduce(function (acc, cur) {
+
+          if (cur.tags.find(function (tag) {
+              return (tag.subject === 'categories' && tag.value === getCountOfReceiptCategoryContext.category);
+            })) {
+            const targetedDocument = cur.notes.find(function (note) {
+              return note.name === 'targetedDocument';
+            });
+            if (targetedDocument) acc.push(targetedDocument);
+          }
+          return acc;
+        }, []);
+        targetedDocumentReqs = targetedDocuments.map(function (doc) {
+          return (Rx.Observable.defer(
+            function () {
+              return rp({
+                method: 'POST',
+                uri: 'http://54.69.169.144:5000/api/v1/bff/xeroDraftDocument',
+                body: doc.value,
+                json: true // Automatically stringifies the body to JSON
+              })
+                .then(function (body) {
+                  return body;
+                });
+            }
+          ))
+        });
+        const createDocsSource = Rx.Observable.forkJoin(targetedDocumentReqs);
+        const subscribe = createDocsSource.subscribe(
+          function (result) {
+            res.json(result);
+          },
+          function (err) {
+            res.json({ error: err });
+          },
+          function () {
+          });
+      }
+    });
+  }
+
   let actionRouter = new Map();
-  
-  // The ASK_WEATHER_INTENT (askWeather) should map to the getWeather method.
-  actionRouter.set(ASK_WEATHER_ACTION, getWeather);
-  
+  actionRouter.set(WELCOME_INPUT, welcome);
+  actionRouter.set(COUNT_NUM, countNumber);
+  actionRouter.set(COUNT_TOTAL, countTotal);
+  actionRouter.set(CREATE_SPEND, createSpend);
+  actionRouter.set(CREATE_SEPND_YES, createSpendYes);
+
   // Route requests to the proper handler functions via the action router.
   assistant.handleRequest(actionRouter);
 });
@@ -110,7 +203,7 @@ function logObject(message, object, options) {
 }
 
 // Listen for requests.
-let server = app.listen(process.env.PORT, function () {
+let server = app.listen(3000, function () {
   console.log('Your app is listening on port ' + server.address().port);
 });
 
